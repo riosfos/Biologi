43624e932f09f66205dfa366223d1787898f0fc6
riosfos <133667573+riosfos@users.noreply.github.com>
2025-02-08 22:14:25 +0700

    Add files via upload
    biologi sigma add

---
+++ asset/main.js
@@ -0,0 +1,36 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+        search.classList.toggle('active');
+        navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+        navbar.classList.toggle('active');
+        search.classList.remove('active');
+}
+
+window.onscroll = () => {
+        navbar.classList.remove('active');
+        search.classList.remove('active');
+}
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+const playButton = document.getElementById('playButton');
+        const music = document.getElementById('music');
+
+        playButton.addEventListener('click', () => {
+            if (music.paused) {
+                music.play();
+            } else {
+                music.pause();
+            }
+        });
